23854833b8aec2ca4817307c99dcf59124537722
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-30 10:48:03 -0500

    created route /feedback and get + post request tested in postman

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,44 @@
+const express = require(`express`);
+const router = express.Router();
+const pool = require(`../modules/pool.js`);
+
+//get request
+router.get(`/`, (req, res) => {
+    console.log(`in /feedback GET`);
+
+    let queryText = `SELECT * FROM "feedback";`;
+
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(`ERROR! Unable to GET from db`, error);
+            res.sendStatus(500);
+        });
+});
+
+
+//post request
+router.post(`/`, (req, res) => {
+    console.log(`in /feedback POST`);
+
+    const feedback = req.body;
+
+    let queryText = `
+        INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+        VALUES ($1, $2, $3, $4);
+    `;
+
+    let values = [feedback.feeling, feedback.understanding, feedback.support, feedback.comments];
+
+    pool.query(queryText, values)
+        .then((result) => {
+            console.log(`/feedback POST successful`);
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(`ERROR! /feedback POST fail!`, error);
+            res.sendStatus(500);
+        });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const feedbackRouter = require(`./routes/feedback.router.js`);
+
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
@@ -9,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use(`/feedback`, feedbackRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
